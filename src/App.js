a9ef022d33d493da7477491e4cea7be0c4dd8c4f
ArsenLen <laurie10@mail.ru>
2023-03-28 20:01:43 +0600

    persons added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header/Header";
+import { useState } from "react";
 
 function App() {
+  //   const course = [
+  //   {
+  //     id: 1,
+  //     name: "IT-RUN React Web Dev",
+  //     tasks : [
+  //       {
+  //         part: "Вводный курс по React",
+  //         task: 7
+  //       },
+  //       {
+  //         part: "Состояние компонента",
+  //         task: 15
+  //       },
+  //       {
+  //         part: "Декомпозиция компонентов",
+  //         task: 10
+  //       },
+  //     ]
+  //   },
+  //   {
+  //     id: 2,
+  //     name: "IT-RUN Python Web Dev",
+  //     tasks : [
+  //       {
+  //         part: "Вводный курс по Python",
+  //         task: 5
+  //       },
+  //       {
+  //         part: "Переменные циклы Python",
+  //         task: 12
+  //       },
+  //       {
+  //         part: "Фреймворк Django",
+  //         task: 17
+  //       },
+  //     ]
+  //   }
+  // ]
+  const [persons, setPersons] = useState([{ name: "Arsen Iusupov" }]);
+
+  const [newName, setNewName] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Пользователи</h1>
+      <form>
+        <input type="text" placeholder="person" />
+        <input type="submit" value="Добавить пользователя" />
+      </form>
+      <div>
+        {/* Тут отобразить всех пользователей в тегах p */}
+      </div>
+
+      {/* <Course /> */}
+      {/* <Header course={course} />
+      <Content tasks={tasks} />
+      <p>Общее количество заданий {task1 + task2 + task3}</p> */}
     </div>
   );
 }
 
 export default App;
+
+/*
+  1. Отобразить пользователей в div. Каждый пользователь в теге p
+  2. При отправке формы, добавлять нового пользователя в состояние persons.
+      2.1 Невозможно добавить пользователя с одинаковым именем и фамилией (arsen iusupov === Arsen Iusupov).
+        Отображать alert, если такой пользователь уже существует
+*/
+
+/*
+    Создать компонент Course, который должен отображать Header, Content, Total каждого курса
+    из массива course.
+    <Course course={course} />
+
+      const Course = () => {
+        return (
+          <>
+            <Header>
+            <Content />
+            <Total>
+          </>
+        )
+      }
+ */
---
+++ src/components/Content/Content.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Part from '../Part/Part';
+
+const Content = (props) => {
+    return (
+        <>
+          {
+            props.tasks.map((task) => <Part part={task.part} task={task.task} />)
+          }
+        </>
+    );
+};
+
+export default Content;
